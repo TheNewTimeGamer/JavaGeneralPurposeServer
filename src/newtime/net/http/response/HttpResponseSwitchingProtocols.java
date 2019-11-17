a25aeafb2750a2d923a7d89f5a0fcd61e46d9c21
TheNewTimeGamer <stefan.voorderhake@live.nl>
2019-11-17 05:28:34 +0100

    Websocket httpresponse body hotfixed

--- src/newtime/net/http/response/HttpResponseSwitchingProtocols.java
+++ src/newtime/net/http/response/HttpResponseSwitchingProtocols.java
@@ -5,7 +5,7 @@ public class HttpResponseSwitchingProtocols extends HttpResponse {
 	public HttpResponseSwitchingProtocols(String wsAcceptKey) {
 
 		this.status = "101 Switching Protocols";
-		this.body = "";
+		this.body = null;
 		this.header.put("Connection", "upgrade");
 		this.header.put("Upgrade", "websocket");
 		this.header.put("Sec-WebSocket-Accept", wsAcceptKey);
