cd1fb77084d58298564c90076d2af3d5fbe6cc4d
Stefan <stefan@chipta.com>
2020-02-26 10:49:54 +0100

    Updated HomeController.

--- src/HomeController.java
+++ src/HomeController.java
@@ -2,15 +2,17 @@ import newtime.net.http.HttpConnection;
 import newtime.net.http.control.Controller;
 import newtime.net.http.request.HttpRequest;
 import newtime.net.http.response.HttpResponse;
-import newtime.net.instancing.Session;
+import newtime.net.http.view.View;
+import newtime.net.http.view.ViewManager;	
 
 public class HomeController extends Controller {
 
 	public static HttpResponse showHomePage(HttpConnection connection, HttpRequest request) {
-		Session session = new Session();
-		session.variables.put("test", "Tim");
-		HttpResponse response = connection.getServerInstance().views.get("home").build(session);		
-		return response;
+		View view = connection.getServerInstance().views.get("home");
+		if(view == null) {
+			return ViewManager.FILE_NOT_FOUND.build();
+		}
+		return view.build();
 	}	
 	
 }
